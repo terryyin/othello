af893807d318a4c373a071ab8d5ff55da473b454
terryyin <terry@odd-e.com>
2013-04-28 22:09:57 +0800

    workable ai

--- test/testAi.py
+++ test/testAi.py
@@ -3,6 +3,25 @@ from .mock import Mock
 from src.othello import ai
 from src.othello.stones import stones
 
+
+def StoneBuilder(pattern):
+    stonesDict = {}
+    available = []
+    y = 0
+    for line in pattern.splitlines():
+        x = 0
+        for c in line.strip():
+            if c == '.':
+                available.append((x, y))
+            else:
+                stonesDict[(x, y)] = int(c)
+            x+=1
+        y+=1
+    stonesObj = stones(stonesDict)
+    stonesObj.availablePositions = set(available)
+    return stonesObj
+
+
 class TestAi(unittest.TestCase):
 
     def testShouldPutAtValidPositionWhenGettingChanceToThink(self):
@@ -10,6 +29,19 @@ class TestAi(unittest.TestCase):
         onPut = Mock()
         me = 1
         currentStones = stones({(0,0):me, (1,0):not me})
+        a.registerPutEvent(onPut)
+        a.think(currentStones, me)
+        pos = onPut.call_args[0][0]
+        self.assertTrue(currentStones.getReversePosistions(pos, me))
+
+    def testShouldPutAtWinningPositionWhenHaving2Choices(self):
+        a = ai()
+        onPut = Mock()
+        me = 1
+        currentStones = StoneBuilder('''
+        10.
+        100.
+        ''')
         a.registerPutEvent(onPut)
         a.think(currentStones, me)
         pos = onPut.call_args[0][0]
